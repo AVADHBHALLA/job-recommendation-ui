5fd6ea2a3366d5e565c0f29717d92a4a42c001d2
AVADHBHALLA <119787467+AVADHBHALLA@users.noreply.github.com>
2026-06-08 11:26:33 +0530

    Initial navbar commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link className="navbar-brand" to="/">
+                    Job Recommendation
+                </Link>
+
+                <div className="navbar-nav ms-auto">
+                    <Link className="nav-link" to="/">
+                        Dashboard
+                    </Link>
+
+                    <Link className="nav-link" to="/users">
+                        Users
+                    </Link>
+
+                    <Link className="nav-link" to="/create-user">
+                        Create User
+                    </Link>
+
+                    <Link className="nav-link" to="/jobs">
+                        Jobs
+                    </Link>
+
+                    <Link className="nav-link" to="/recommendations">
+                        Recommendations
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
